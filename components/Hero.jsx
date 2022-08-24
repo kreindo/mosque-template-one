2f5a81b89dbcd567824349f2176dd5cb723a7d0b
kreindo <ahmadsan4p@gmail.com>
2022-08-24 08:01:06 +0800

    init mosque project

---
+++ components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <>
+      <Layout>
+        {/* <Header logo={} menus={}> */}
+      </Layout>
+      
+    </>
+  );
+}
+
+export default Hero;
