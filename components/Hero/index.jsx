8f7b2d05a82705de29b882ec6ca85fb9bf17654a
kreindo <ahmadsan4p@gmail.com>
2022-08-25 18:00:01 +0800

    finishing for the day

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,17 +1,23 @@
-import Image from 'next/image';
-import logoImage from '../../src/assets/images/logo.png';
+import Image from 'next/image';import logoImage from '../../src/assets/images/logo.png';
 import Header from '../Header';
+import Intersect from '../../src/assets/images/Intersect.svg';
 import HeroTwoBlock from '../../src/assets/images/HeroTwoBlock.svg';
+import MosqueHeroImg from '../../src/assets/images/clipmosque.svg';
+import Mosque from '../../src/assets/images/mosque.png';
 export default function Hero() {
   return (
     <>
-      <div className="z-[-1] lg:w-[1235px] h-[431px] bg-warna-primary-maroon absolute left-[250px] top-[-346px] rotate-[10deg]"></div>
+      <div className="absolute top-0 right-0 z-[-1]">
+        <Image src={Intersect} alt={'Intersect cube'} />
+      </div>
       <div className="lg:mt-[100px] grid grid-cols-2 gap-[150px]">
         <div className="hidden lg:block">
+          <div>test</div>
+          <Image src={MosqueHeroImg} alt={'Hero Mosque Image'} />
           <Image src={HeroTwoBlock} alt={'Hero image'} />
         </div>
         <div className="flex flex-col w-[70%] justify-center">
-          <h1 className="text-[24px] mb-[10px] leading-[56px] lg: overflo text-[48px] font-extrabold">
+          <h1 className="text-[24px] mb-[10px] leading-[56px] lg:text-[48px] font-extrabold">
             Welcome to Masjid Amirul Mu’awanah Landing Page
           </h1>
           <div className="lg:w-[125px] h-[5px] bg-[black]"></div>
