e3ee0b0a271fd958e92d5b4312a350914fcb8822
kreindo <ahmadsan4p@gmail.com>
2022-08-28 18:11:47 +0800

    init fetch

---
+++ components/Slider/index.jsx
@@ -0,0 +1,14 @@
+import SliderItem from './SliderItem';
+const Index = () => {
+  const arr = ['poster 1', 'poster 2', 'poster 3'];
+  return (
+    <div className="mt-[10em] flex">
+      {arr.map((arrDat) => {
+        return <SliderItem key={arrDat} iteration={arrDat.index} />;
+      })}
+      <SliderItem />
+    </div>
+  );
+};
+
+export default Index;
