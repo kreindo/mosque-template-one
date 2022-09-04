49aae8395f45aabf18d0a9418f0fd1b53d422fdd
kreindo <ahmadsan4p@gmail.com>
2022-09-04 18:09:22 +0800

    finalize for the day, refining carousel

--- pages/Kontak/index.js
+++ pages/Kontak/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import Layout from '../../components/Layout';
-
 const Index = () => {
   return (
     <Layout title={"Kontak | Masjid Mu'awanah"} active={'Kontak'}>
-      <div>Contact Page</div>
+      <div className="flex"></div>
     </Layout>
   );
 };
