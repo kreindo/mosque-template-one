647830ffc7781bb56d030b1cfadc491b6fbba59b
kreindo <ahmadsan4p@gmail.com>
2022-09-06 16:23:14 +0800

    lifting props

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';import Heading from '../Heading';
 import MosqueHeroImg from '../../src/assets/images/imgHero5.png';
-export default function Hero() {
+export default function Hero({ Masjid }) {
   return (
     <>
       <div className="flex lg:mt-[50px] gap-[150px]">
@@ -8,7 +8,7 @@ export default function Hero() {
           <Image src={MosqueHeroImg} alt={'Hero Mosque Image'} />
           {/* <Image src={HeroTwoBlock} alt={'Hero image'} /> */}
         </div>
-        <Heading withButton title={"Welcome to Masjid Mu'awanah Landing Page"}>
+        <Heading withButton title={`Welcome to ${Masjid} Landing Page`}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
           natus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
           molestias corporis dolorum quam cum facilis aperiam dolores iusto
